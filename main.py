b07efe9bb2187c66e3d6ef20e255a056291e69c1
Babawande <68830561+wand3@users.noreply.github.com>
2023-12-15 05:00:31 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-#!/usr/bin/env python3
-import asyncio
+#!\usr\bin\python3
+import time
 from playwright.sync_api import Playwright, sync_playwright, expect
-
 
 def run(playwright: Playwright) -> None:
     browser = playwright.chromium.launch(headless=False)
@@ -14,14 +13,14 @@ def run(playwright: Playwright) -> None:
     context = browser.new_context(storage_state="state.json") 
 
 
-
     # goto sporty page and login
-    page.goto("https://www.google.com/search?q=sportyng&oq=sportyng&gs_lcrp=EgZjaHJvbWUyBggAEEUYOdIBCTEwMDY3ajBqMagCALACAA&sourceid=chrome&ie=UTF-8")
-    page.get_by_role("link", name="Login", exact=True).click()
-    page.goto("https://www.sportybet.com/ng/m/")
+    page.goto("https://www.sportybet.com/ng", timeout=60000)
     page.locator("input[name=\"phone\"]").fill("8159348152")
+    time.sleep(2)    
+
     page.get_by_placeholder("Password").click()
     page.get_by_placeholder("Password").fill("/Poiuytrewq123")
+    time.sleep(2)
     page.get_by_role("button", name="Login").click(force=True)
 
 
@@ -32,45 +31,43 @@ def run(playwright: Playwright) -> None:
     # page.locator(".icon-cross").click()
     # page.get_by_text("Bets").click()
     # page.get_by_text("Winnings").click()
-    # page.get_by_text("Bets").click()
-    page.locator("#header_nav_sports").click()
     page.get_by_placeholder("Booking Code").click()
-    page.get_by_placeholder("Booking Code").fill("0687d016")
-    page.get_by_role("button", name="Load").click()
-    page.get_by_placeholder("min.").click()
-    # page.get_by_placeholder("min.").dblclick()
-    # page.get_by_placeholder("min.").press("ArrowRight")
-    # page.get_by_placeholder("min.").press("ArrowRight")
-    page.get_by_placeholder("min.").fill("10")
-    page.get_by_text("Load", exact=True).click()
-        
-    page.get_by_role("button", name="Place Bet").click()
-    page.get_by_role("button", name="Confirm").click()
-    page.get_by_role("button", name="OK").click()
-    # page.get_by_role("button", name="Place Bet").click()
-    # await page.get_by_role("button", name="Confirm").click()
-    # page.get_by_role("button", name="Cancel").click()
-    # page.get_by_role("button", name="Place Bet").click()
-    # page.get_by_role("button", name="Cancel").click()
-    # page.locator("div:nth-child(31) > div:nth-child(2) > .m-lay-mid > .m-item-play").click()
+    
+    # loop for placing values
+    numbers = ["c53d424", "92d558b7", "21197200"]
+    count = 0
+    while count <= len(numbers):
+        for i in range(len(numbers)):
+            place = numbers[i]
+            page.get_by_placeholder("Booking Code").fill(f'{place}')
+            page.get_by_role("button", name="Load").click()
+            page.get_by_placeholder("min.").click()
+            time.sleep(9) 
+            # page.get_by_placeholder("min.").dblclick()
+            # page.get_by_placeholder("min.").press("ArrowRight")
+            # page.get_by_placeholder("min.").press("ArrowRight")
+            page.get_by_placeholder("min.").fill("10")
+            page.get_by_text("Load", exact=True).click()
+            page.get_by_role("button", name="Place Bet").click()
+            page.get_by_text("Accept Changes").click()
+
+            page.get_by_role("button", name="Confirm").click()
+            page.get_by_role("button", name="OK").click()
+            page.get_by_text("Sports").click()
+
+
+
+    # page.get_by_placeholder("Booking Code").fill("")
+    # page.get_by_role("button", name="Load").click()
     # page.get_by_placeholder("min.").click()
-    # page.get_by_placeholder("min.").press("ArrowRight")
-    # page.get_by_placeholder("min.").press("Enter")
-    # page.get_by_placeholder("min.").click()
-    # page.get_by_placeholder("min.").press("ArrowRight")
-    # page.get_by_placeholder("min.").press("ArrowRight")
-    # page.get_by_placeholder("min.").fill("15")
-    # page.get_by_role("button", name="Place Bet").click()
-    # page.get_by_role("button", name="Place Bet").click()
-    # page.get_by_role("button", name="Confirm").click()
-    # page.locator("#esDialog1").get_by_role("img").first.click()
-    # page.get_by_placeholder("min.").click()
-    page.goto("https://www.sportybet.com/ng/m/")
-    # page.get_by_placeholder("Booking Code").click()
-    # page.get_by_placeholder("Booking Code").click()
-    # page.get_by_placeholder("Booking Code").dblclick()
-    # page.get_by_placeholder("Booking Code").fill("0687d016")
+    # time.sleep(9) 
+    # # page.get_by_placeholder("min.").dblclick()
+    # # page.get_by_placeholder("min.").press("ArrowRight")
+    # # page.get_by_placeholder("min.").press("ArrowRight")
+    # page.get_by_placeholder("min.").fill("10")
     # page.get_by_text("Load", exact=True).click()
+        
+    # page.get_by_role("button", name="Place Bet").click()
     # page.get_by_role("button", name="Confirm").click()
     # page.get_by_role("button", name="OK").click()
 
